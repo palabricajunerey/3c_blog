fd16149123b7d26b89bdad65b81615e7fa95d9d8
palabricajunerey <dloadall1@gmail.com>
2022-12-01 15:44:48 +0800

    added sql

--- 3c_blogs.sql
+++ 3c_blogs.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 29, 2022 at 05:06 AM
+-- Generation Time: Dec 01, 2022 at 08:44 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -30,17 +30,44 @@ SET time_zone = "+00:00";
 CREATE TABLE `blogs` (
   `blog_id` int(11) NOT NULL,
   `blog_title` varchar(255) NOT NULL,
-  `blog_content` varchar(255) NOT NULL
+  `blog_content` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `blogs`
 --
 
-INSERT INTO `blogs` (`blog_id`, `blog_title`, `blog_content`) VALUES
-(1, 'PHP 5', 'is a server-side programming language, is updated'),
-(2, 'Sample', 'sample123'),
-(4, 'Java', 'java is java');
+INSERT INTO `blogs` (`blog_id`, `blog_title`, `blog_content`, `user_id`, `created_at`) VALUES
+(1, 'PHP 5', 'is a server-side programming language, is updated', 0, '2022-12-01 07:25:57'),
+(4, 'Java', 'java is java', 0, '2022-12-01 07:25:57'),
+(8, 'JavaScript', 'JS', 0, '2022-12-01 07:25:57'),
+(9, 'Kay cruz', 'sample content ni cruz', 2, '2022-12-01 07:28:27'),
+(10, 'kay cruz gyapon', 'sample content ni cruz', 2, '2022-12-01 07:29:09'),
+(11, 'Kay juan ni', 'kay juan content', 4, '2022-12-01 07:32:13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `first_name`, `last_name`, `email`, `password`) VALUES
+(2, 'Juanita', 'Cruz', 'cruz@gmail.com', '$2y$10$v5rk4BVJOSdoPDTR3RNR2OFJtdyRh9CB0OJ6tqC0q5iprcZwwZ.ky'),
+(4, 'Juan', 'Dela Cruz', 'juan@gmail.com', '$2y$10$umXCgc8BF86GLbVe/Tso2eZ7V77Vxh8R9GZwO3nrp9xW4epv9mgiO');
 
 --
 -- Indexes for dumped tables
@@ -53,6 +80,12 @@ ALTER TABLE `blogs`
   ADD PRIMARY KEY (`blog_id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -60,7 +93,13 @@ ALTER TABLE `blogs`
 -- AUTO_INCREMENT for table `blogs`
 --
 ALTER TABLE `blogs`
-  MODIFY `blog_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `blog_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
